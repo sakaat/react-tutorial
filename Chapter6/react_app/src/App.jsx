207b4630a6a9b08632bccf4a36af6478d7bd74df
sakata.tomoyuki <a1819ts@aiit.ac.jp>
2020-04-14 00:41:56 +0900

    6-1 Firebaseを使ってみよう

--- Chapter6/react_app/src/App.jsx
+++ Chapter6/react_app/src/App.jsx
@@ -1,38 +1,31 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 import "./App.css";
-import Memo from "./memo/Memo";
-import AddForm from "./memo/AddForm";
-import FindForm from "./memo/FindForm";
-import DelForm from "./memo/DelForm";
+import firebase from "firebase";
+import Sampledata from "./fire/Sampledata";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqJBX8pZzyJgGnWxm_KBOjc7_M6JsRjY8",
+    authDomain: "react-tutorial-f2f2d.firebaseapp.com",
+    databaseURL: "https://react-tutorial-f2f2d.firebaseio.com",
+    projectId: "react-tutorial-f2f2d",
+    storageBucket: "react-tutorial-f2f2d.appspot.com",
+    messagingSenderId: "147929545398",
+    appId: "1:147929545398:web:acb813afa2a474e69ec417",
+    measurementId: "G-0DXZQY2GYR",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
-    td = {
-        width: "250px",
-    };
-
     render() {
         return (
             <div>
-                <h1>Memo</h1>
-                <AddForm />
-                <hr />
-                <table>
-                    <tbody>
-                        <tr>
-                            <td style={this.td}>
-                                <FindForm />
-                            </td>
-                            <td style={this.td}>
-                                <DelForm />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <Memo />
+                <h1>Fire</h1>
+                <h2>Sample data.</h2>
+                <Sampledata />
             </div>
         );
     }
 }
 
-export default connect()(App);
+export default App;
